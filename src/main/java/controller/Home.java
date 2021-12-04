1ba6b08508e3b2caf7a6448e67d2f8560c675e72
wj601653489 <wj8621174>
2021-12-05 05:11:38 +0800

    增加用户本地配置文件保存功能

--- src/main/java/controller/Home.java
+++ src/main/java/controller/Home.java
@@ -9,68 +9,120 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class Home implements Initializable {
 
     /**
+     * 本地配置文件路径
+     */
+    private Path configLocation;
+
+    /**
      * 配置文件
      */
     private Document document;
-    private File xmlFile;
-    private Element jira_url;
-    private Element jira_jql;
-    private Element notion_token;
-    private Element notion_databaseID;
 
-
+    /**
+     * 界面显示字段
+     */
     public TextField jiraUrl;
     public TextField jiraJql;
     public TextField notionToken;
     public TextField notionDatabaseId;
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {
-            document = new SAXReader().read(getClass().getClassLoader().getResourceAsStream("config.xml"));
-            xmlFile = new File(getClass().getClassLoader().getResource("config.xml").toURI());
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        // 获取XML对象
-        Element rootElement = document.getRootElement();
-        jira_url = rootElement.element("jira").element("url");
-        jira_jql = rootElement.element("jira").element("jql");
-        notion_token = rootElement.element("notion").element("token");
-        notion_databaseID = rootElement.element("notion").element("databaseID");
+        // 用户本地配置文件路径
+        configLocation = Paths.get(System.getProperty("user.home"), ".jiraToNotion", "config.xml");
 
-        // 界面设置值
-        jiraUrl.setText(jira_url.getText());
-        jiraJql.setText(jira_jql.getText());
-        notionToken.setText(notion_token.getText());
-        notionDatabaseId.setText(notion_databaseID.getText());
+        // 本地配置文件是否存在
+        if (Files.exists(configLocation)) {
+            // 读取本地配置文件
+            try (BufferedReader bufferedReader = Files.newBufferedReader(configLocation)) {
+                document = new SAXReader().read(bufferedReader);
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // 界面设置值
+            Element rootElement = document.getRootElement();
+            jiraUrl.setText(rootElement.element("jira").element("url").getText());
+            jiraJql.setText(rootElement.element("jira").element("jql").getText());
+            notionToken.setText(rootElement.element("notion").element("token").getText());
+            notionDatabaseId.setText(rootElement.element("notion").element("databaseID").getText());
+        } else {
+            // 配置文件父目录不存在
+            if (!Files.exists(configLocation.getParent())) {
+                // 创建父目录
+                try {
+                    Files.createDirectory(configLocation.getParent());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // 文件不存在就从jar包里将默认的配置文件复制到本地
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/config.xml"))); BufferedWriter out = Files.newBufferedWriter(configLocation)) {
+                in.lines().forEach(line -> {
+                    try {
+                        out.append(line);
+                        out.newLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void jira_save(ActionEvent actionEvent) {
-        jira_url.setText(jiraUrl.getText());
-        jira_jql.setText(jiraJql.getText());
-        try {
-            XMLWriter writer = new XMLWriter(new FileWriter(xmlFile));
-            writer.write(document);
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // 设置修改后的值
+        document.getRootElement().element("jira").element("url").setText(jiraUrl.getText());
+        document.getRootElement().element("jira").element("jql").setText(jiraJql.getText());
+
+        saveConfig();
     }
 
     public void notion_save(ActionEvent actionEvent) {
+        // 设置修改后的值
+        document.getRootElement().element("notion").element("token").setText(notionToken.getText());
+        document.getRootElement().element("notion").element("databaseID").setText(notionDatabaseId.getText());
 
+        saveConfig();
+    }
+
+    /**
+     * 保存到本地配置文件
+     */
+    private void saveConfig() {
+        XMLWriter writer = null;
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(configLocation)) {
+            writer = new XMLWriter(bufferedWriter);
+            writer.write(document);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
